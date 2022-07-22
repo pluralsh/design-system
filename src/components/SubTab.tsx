a526b74207c6d524492324590ef02af53245fbf3
Klink <85062+dogmar@users.noreply.github.com>
2022-07-22 14:31:04 -0700

    Added SubTab

---
+++ src/components/SubTab.tsx
@@ -0,0 +1,39 @@
+import { ReactNode, Ref, forwardRef } from 'react'
+import { DivProps, Flex, Icon } from 'honorable'
+
+type SubTabProps = DivProps & {
+  active?: boolean
+  startIcon?: ReactNode
+  vertical?: boolean
+}
+
+const SubTab = forwardRef(
+  (
+    { startIcon, active, children, vertical, ...props }: SubTabProps,
+    ref: Ref<any>
+  ) => (
+    <Flex
+      ref={ref}
+      buttonMedium
+      tabIndex={0}
+      userSelect="none"
+      cursor="pointer"
+      textAlign="center"
+      paddingVertical="xsmall"
+      paddingHorizontal="medium"
+      borderRadius="medium"
+      color={active ? 'text' : 'text-xlight'}
+      backgroundColor={active ? 'fill-zero-selected' : 'none'}
+      _hover={{
+        color: 'text',
+        backgroundColor: active ? 'fill-zero-selected' : 'action-input-hover',
+      }}
+      {...props}
+    >
+      {!!startIcon && <Icon marginRight="small">{startIcon}</Icon>}
+      {children}
+    </Flex>
+  )
+)
+
+export default SubTab
