69dc5d58724785897179b2e298c198b739e4fd27
Marcin Maciaszczyk <marcin@plural.sh>
2022-09-08 12:12:32 +0200

    Remove duplicated code

--- src/stories/Highlight.stories.tsx
+++ src/stories/Highlight.stories.tsx
@@ -1,41 +1,8 @@
 import { Flex } from 'honorable'
 
+import { goCode, jsCode, tfCode } from '../constants'
+
 import { Divider, Highlight } from '..'
-
-const goCode = `package main
-
-import "fmt"
-          
-func main() {
-  fmt.Println("Hello, 世界")
-}`
-
-const jsCode = `function reverseString(str) {
-  let newString = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-      newString += str[i];
-  }
-  return newString;
-}`
-
-const tfCode = `vpc_name = {{ .Values.vpc_name | quote }}
-cluster_name = {{ .Cluster | quote }}
-
-map_roles = [
-  {
-    rolearn = "arn:aws:iam::{{ .Project }}:role/{{ .Cluster }}-console"
-    username = "console"
-    groups = ["system:masters"]
-  }
-]
-
-
-{{- if .Values.database_subnets }}
-database_subnets = yamldecode(<<EOT
-{{ .Values.database_subnets | toYaml }}
-EOT
-)
-{{- end }}`
 
 export default {
   title: 'Highlight',
