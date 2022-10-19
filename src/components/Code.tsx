f6d2fb24642b936869401931d61691ad433aad6b
Klink <85062+dogmar@users.noreply.github.com>
2022-10-19 16:35:59 -0700

    Update Code styling
    + Add `showLineNumbers` option

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -1,27 +1,85 @@
 import {
   RefObject, forwardRef, useEffect, useState,
 } from 'react'
-import {
-  Button, Div, Flex, FlexProps,
-} from 'honorable'
+import { Button, Div, Flex } from 'honorable'
+
+import styled from 'styled-components'
 
 import CopyIcon from './icons/CopyIcon'
-import Card from './Card'
+import Card, { CardProps } from './Card'
 import CheckIcon from './icons/CheckIcon'
 import Highlight from './Highlight'
+import { FillLevel, useFillLevel } from './contexts/FillLevelContext'
+import FileIcon from './icons/FileIcon'
 
-type CodeProps = FlexProps & {
-  children: string,
-  language?: string,
+type CodeProps = Omit<CardProps, 'children'> & {
+  children: string
+  language?: string
+  showLineNumbers?: boolean
 }
 
 const propTypes = {}
 
-function CodeRef({ children, language, ...props }: CodeProps, ref: RefObject<any>) {
+const CodeHeader = styled.div<{ fillLevel: FillLevel }>(({ fillLevel, theme }) => ({
+  ...theme.partials.text.overline,
+  minHeight: theme.spacing.xlarge + theme.spacing.xsmall * 2,
+  padding: `${theme.spacing.xsmall}px ${theme.spacing.medium}px`,
+  borderBottom:
+      fillLevel >= 1 ? theme.borders['fill-three'] : theme.borders['fill-two'],
+  color: 'text-light',
+  backgroundColor:
+      fillLevel >= 1 ? theme.colors['fill-three'] : theme.colors['fill-two'],
+  display: 'flex',
+  alignItems: 'center',
+  gap: theme.spacing.xsmall,
+}))
+
+function CopyButtonBase({
+  copied,
+  handleCopy,
+  className,
+}: {
+  copied: boolean
+  handleCopy: () => any
+  className?: string
+}) {
+  return (
+    <Button
+      className={className}
+      position="absolute"
+      floating
+      small
+      startIcon={copied ? <CheckIcon /> : <CopyIcon />}
+      onClick={handleCopy}
+    >
+      {copied ? 'Copied' : 'Copy'}
+    </Button>
+  )
+}
+const CopyButton = styled(CopyButtonBase)<{ verticallyCenter: boolean }>(({ verticallyCenter, theme }) => ({
+  position: 'absolute',
+  right: theme.spacing.medium,
+  top: verticallyCenter ? '50%' : theme.spacing.medium,
+  transform: verticallyCenter ? 'translateY(-50%)' : 'none',
+  boxShadow: theme.boxShadows.slight,
+}))
+
+function CodeRef({
+  children, language, showLineNumbers, ...props
+}: CodeProps,
+ref: RefObject<any>) {
   const [copied, setCopied] = useState(false)
   const [hover, setHover] = useState(false)
+  const fillLevel = useFillLevel()
 
-  if (typeof children !== 'string') throw new Error('Code component expects a string as its children')
+  if (typeof children !== 'string') {
+    throw new Error('Code component expects a string as its children')
+  }
+
+  const showHeader = !!language
+
+  children = children.trim()
+  const multiLine = !!children.match(/\r?\n/)
 
   useEffect(() => {
     if (copied) {
@@ -45,42 +103,41 @@ function CodeRef({ children, language, ...props }: CodeProps, ref: RefObject<any
         direction="column"
         height="100%"
       >
-        {!!language && (
-          <Div
-            paddingHorizontal="large"
-            paddingVertical="medium"
-            borderBottom="1px solid border"
-            overline
-            color="text-light"
-          >
-            {language}
-          </Div>
+        {showHeader && (
+          <CodeHeader fillLevel={fillLevel}>
+            <FileIcon />
+            <div>{language}</div>
+          </CodeHeader>
         )}
         <Div
-          minHeight="90px"
+          position="relative"
           height="100%"
-          overflow="auto"
-          alignItems="center"
+          overflow="hidden"
         >
-          {hover && (
-            <Button
-              small
-              position="absolute"
-              right="24px"
-              top={language ? '73px' : '24px'}
-              floating
-              startIcon={copied ? <CheckIcon /> : <CopyIcon />}
-              onClick={handleCopy}
-            >
-              {copied ? 'Copied' : 'Copy'}
-            </Button>
-          )}
-          <Highlight
-            language={language}
-            padding="large"
+          <Div
+            height="100%"
+            overflow="auto"
+            alignItems="center"
           >
-            {children}
-          </Highlight>
+            {hover && (
+              <CopyButton
+                copied={copied}
+                handleCopy={handleCopy}
+                verticallyCenter={!multiLine}
+              />
+            )}
+            <Div
+              paddingHorizontal="medium"
+              paddingVertical={multiLine ? 'medium' : 'small'}
+            >
+              <Highlight
+                showLineNumbers={showLineNumbers}
+                language={language}
+              >
+                {children}
+              </Highlight>
+            </Div>
+          </Div>
         </Div>
       </Flex>
     </Card>
