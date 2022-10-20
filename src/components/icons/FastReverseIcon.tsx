a7c6bd7a5ff569146fe4275885df13aec11d8adc
Klink <85062+dogmar@users.noreply.github.com>
2022-10-20 10:31:03 -0700

    Replace icons with transformed versions of other icons to save bundle size

--- src/components/icons/FastReverseIcon.tsx
+++ src/components/icons/FastReverseIcon.tsx
@@ -1,19 +1,9 @@
-import createIcon from './createIcon'
+import styled from 'styled-components'
 
-export default createIcon(({ size, color }) => (
-  <svg
-    width={size}
-    viewBox="0 0 16 16"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <g
-      fill="none"
-      stroke={color}
-      strokeMiterlimit="10"
-    >
-      <path d="m.9515 8.4484 6.289 3.924c.255.157.54-.097.54-.485v-7.775c0-.382-.285-.643-.54-.485l-6.289 3.851c-.303.188-.303.782 0 .976z" />
-      <path d="m8.4472 8.4484 3.145 1.962 3.144 1.962c.255.157.54-.097.54-.485v-7.775c0-.382-.285-.643-.54-.485l-6.289 3.851c-.303.188-.303.782 0 .976z" />
-    </g>
-  </svg>
-))
+import FastForwardIcon from './FastForwardIcon'
+
+export default styled(FastForwardIcon)(_ => ({
+  svg: {
+    transform: 'scaleX(-1)',
+  },
+}))
--- src/components/icons/ThumbsDownIcon.tsx
+++ src/components/icons/ThumbsDownIcon.tsx
@@ -1,29 +1,9 @@
-import createIcon from './createIcon'
+import styled from 'styled-components'
 
-export default createIcon(({ size, color }) => (
-  <svg
-    width={size}
-    viewBox="0 0 16 16"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <g
-      fill="none"
-      stroke={color}
-      strokeMiterlimit="10"
-    >
-      <rect
-        height="7.6"
-        rx=".5"
-        strokeWidth=".89"
-        width="3.1"
-        x="12.2394"
-        y="1.1843"
-      />
-      <path
-        d="m11.1394 7.6844v-5.9h-.9s-5.3-1.7-7.5-.5c0 0-.7.3-1.1 1.1l-.9 5.6s-.6 1.9 2 1.7l2.5-.5s-.6 1.8-.9 2.4c-1 1.8-1.5 4.4 1 3.2.5-.4 2.8-4.5 2.8-4.5s1.1-1.9 1.8-2.1c.7-.1 1.2-.2 1.2-.5z"
-        strokeWidth="1.04"
-      />
-    </g>
-  </svg>
-))
+import ThumbsUpIcon from './ThumbsUpIcon'
+
+export default styled(ThumbsUpIcon)(_ => ({
+  svg: {
+    transform: 'rotate(180deg)',
+  },
+}))
