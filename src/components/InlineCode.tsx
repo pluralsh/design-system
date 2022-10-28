44d094c6583ab9688b6d12e9007c0f87b4d679f3
Klink <85062+dogmar@users.noreply.github.com>
2022-10-28 09:09:46 -0700

    Remove debug code

--- src/components/InlineCode.tsx
+++ src/components/InlineCode.tsx
@@ -35,7 +35,7 @@ const parentFillLevelToBorderColor: {
 
 // Magic number to make the total height equal 20px when within 14px type
 // Proportional to that for larger text sizes
-// If INLINE_CODE_MIN_PX changes in them/text.ts, this needs to change too
+// If INLINE_CODE_MIN_PX changes in theme/text.ts, this needs to change too
 // I cannot for the life of me find a way to just calculate this in a way that's
 // consistent when INLINE_CODE_MIN_PX changes.
 const PADDING_EMS = 0.1669
@@ -68,7 +68,6 @@ const InlineCode = forwardRef<HTMLElement, ComponentPropsWithRef<'code'>>(({ ...
   return (
     <>
       <Spacer />
-      {parentFillLevel}
       <Code
         ref={ref}
         parentFillLevel={parentFillLevel}
