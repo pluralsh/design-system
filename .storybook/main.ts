e19d40374dfb8745f04e8c923647d68625c69791
David van der Spek <vanderspek.david@gmail.com>
2022-11-18 18:23:18 +0100

    fix: set correct builder-vite version
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,13 @@ import type { StorybookViteConfig } from '@storybook/builder-vite';
 
 const config: StorybookViteConfig = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-docs', '@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/preset-create-react-app'],
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-create-react-app',
+  ],
   framework: {
     name: '@storybook/react-vite',
     options: {}
