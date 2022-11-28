44ba4cf59e39d95d9e5e38e3cc57c1a0b1582085
Sebastian Florek <s.florek91@gmail.com>
2022-11-28 12:18:35 +0100

    Fix eslint config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,14 @@
-import type { StorybookConfig } from '@storybook/builder-vite';
+import { StorybookConfig } from '@storybook/builder-vite'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
-    name: '@storybook/react-vite'
+    name: '@storybook/react-vite',
   },
   core: {
-    builder: '@storybook/builder-vite'
+    builder: '@storybook/builder-vite',
   },
-};
+}
 
-export default config;
+export default config
