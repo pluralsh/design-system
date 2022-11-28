f77d8327690b2b6b28ed1a7919be0f61db84de5c
Sebastian Florek <s.florek91@gmail.com>
2022-11-28 11:36:20 +0100

    Reconfigure vite as per official migration guide

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,42 +2,12 @@ import type { StorybookConfig } from '@storybook/builder-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-docs',
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
-    name: '@storybook/react-vite',
-    options: {}
+    name: '@storybook/react-vite'
   },
   core: {
-    builder: '@storybook/builder-vite',
-  },
-  // docsPage: {
-  //   docs: 'automatic',
-  // },
-  async viteFinal(config) {
-    return {
-      ...config,
-      esbuild: {
-        ...config.esbuild,
-        jsxInject: `import React from 'react'`,
-      },
-      rollupOptions: {
-        ...config,
-        // Externalize deps that shouldn't be bundled
-        external: ["react", "react-dom"],
-        output: {
-          // Global vars to use in UMD build for externalized deps
-          globals: {
-            react: "React",
-            "react-dom": "ReactDOM",
-          },
-        },
-      },
-    };
+    builder: '@storybook/builder-vite'
   },
 };
 
