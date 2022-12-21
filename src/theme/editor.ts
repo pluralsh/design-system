44e9b460ea6c1be40f97aeb1aa6c5c996040162a
Marcin Maciaszczyk <marcin@plural.sh>
2022-12-21 11:41:59 +0100

    Update editor theme

--- src/theme/editor.ts
+++ src/theme/editor.ts
@@ -5,323 +5,323 @@ export const editorTheme = {
   base: 'vs-dark',
   rules: [
     {
-      foreground: '#DCDCAA',
+      foreground: '#FFF9C2',
       token: 'entity.name.function',
     },
     {
-      foreground: '#DCDCAA',
+      foreground: '#FFF9C2',
       token: 'support.function',
     },
     {
-      foreground: '#DCDCAA',
+      foreground: '#FFF9C2',
       token: 'support.constant.handlebars',
     },
     {
-      foreground: '#DCDCAA',
+      foreground: '#FFF9C2',
       token: 'source.powershell variable.other.member',
     },
     {
-      foreground: '#DCDCAA',
+      foreground: '#FFF9C2',
       token: 'entity.name.operator.custom-literal',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'meta.return-type',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'support.class',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'support.type',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'entity.name.type',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'entity.name.namespace',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'entity.other.attribute',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'entity.name.scope-resolution',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'entity.name.class',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.numeric.go',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.byte.go',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.boolean.go',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.string.go',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.uintptr.go',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.error.go',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.rune.go',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.cs',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.generic.cs',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.modifier.cs',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.variable.cs',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.annotation.java',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.generic.java',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.java',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.object.array.java',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.primitive.array.java',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.primitive.java',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.token.java',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.groovy',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.annotation.groovy',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.parameters.groovy',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.generic.groovy',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.object.array.groovy',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.primitive.array.groovy',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'storage.type.primitive.groovy',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'meta.type.cast.expr',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'meta.type.new.expr',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'support.constant.math',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'support.constant.dom',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'support.constant.json',
     },
     {
-      foreground: '#4EC9B0',
+      foreground: '#99F5D5',
       token: 'entity.other.inherited-class',
     },
     {
-      foreground: '#C586C0',
+      foreground: '#969AF8',
       token: 'keyword.control',
     },
     {
-      foreground: '#C586C0',
+      foreground: '#969AF8',
       token: 'source.cpp keyword.operator.new',
     },
     {
-      foreground: '#C586C0',
+      foreground: '#969AF8',
       token: 'keyword.operator.delete',
     },
     {
-      foreground: '#C586C0',
+      foreground: '#969AF8',
       token: 'keyword.other.using',
     },
     {
-      foreground: '#C586C0',
+      foreground: '#969AF8',
       token: 'keyword.other.operator',
     },
     {
-      foreground: '#C586C0',
+      foreground: '#969AF8',
       token: 'entity.name.operator',
     },
     {
-      foreground: '#9CDCFE',
+      foreground: '#C2E9FF',
       token: 'variable',
     },
     {
-      foreground: '#9CDCFE',
+      foreground: '#C2E9FF',
       token: 'meta.definition.variable.name',
     },
     {
-      foreground: '#9CDCFE',
+      foreground: '#C2E9FF',
       token: 'support.variable',
     },
     {
-      foreground: '#9CDCFE',
+      foreground: '#C2E9FF',
       token: 'entity.name.variable',
     },
     {
-      foreground: '#4FC1FF',
+      foreground: '#66C7FF',
       token: 'variable.other.constant',
     },
     {
-      foreground: '#4FC1FF',
+      foreground: '#66C7FF',
       token: 'variable.other.enummember',
     },
     {
-      foreground: '#9CDCFE',
+      foreground: '#C2E9FF',
       token: 'meta.object-literal.key',
     },
     {
-      foreground: '#CE9178',
+      foreground: '#D596F4',
       token: 'support.constant.property-value',
     },
     {
-      foreground: '#CE9178',
+      foreground: '#D596F4',
       token: 'support.constant.font-name',
     },
     {
-      foreground: '#CE9178',
+      foreground: '#D596F4',
       token: 'support.constant.media-type',
     },
     {
-      foreground: '#CE9178',
+      foreground: '#D596F4',
       token: 'support.constant.media',
     },
     {
-      foreground: '#CE9178',
+      foreground: '#D596F4',
       token: 'constant.other.color.rgb-value',
     },
     {
-      foreground: '#CE9178',
+      foreground: '#D596F4',
       token: 'constant.other.rgb-value',
     },
     {
-      foreground: '#CE9178',
+      foreground: '#D596F4',
       token: 'support.constant.color',
     },
     {
-      foreground: '#CE9178',
+      foreground: '#D596F4',
       token: 'punctuation.definition.group.regexp',
     },
     {
-      foreground: '#CE9178',
+      foreground: '#D596F4',
       token: 'punctuation.definition.group.assertion.regexp',
     },
     {
-      foreground: '#CE9178',
+      foreground: '#D596F4',
       token: 'punctuation.definition.character-class.regexp',
     },
     {
-      foreground: '#CE9178',
+      foreground: '#D596F4',
       token: 'punctuation.character.set.begin.regexp',
     },
     {
-      foreground: '#CE9178',
+      foreground: '#D596F4',
       token: 'punctuation.character.set.end.regexp',
     },
     {
-      foreground: '#CE9178',
+      foreground: '#D596F4',
       token: 'keyword.operator.negation.regexp',
     },
     {
-      foreground: '#CE9178',
+      foreground: '#D596F4',
       token: 'support.other.parenthesis.regexp',
     },
     {
-      foreground: '#d16969',
+      foreground: '#BE5EEB',
       token: 'constant.character.character-class.regexp',
     },
     {
-      foreground: '#d16969',
+      foreground: '#BE5EEB',
       token: 'constant.other.character-class.set.regexp',
     },
     {
-      foreground: '#d16969',
+      foreground: '#BE5EEB',
       token: 'constant.other.character-class.regexp',
     },
     {
-      foreground: '#d16969',
+      foreground: '#BE5EEB',
       token: 'constant.character.set.regexp',
     },
     {
-      foreground: '#DCDCAA',
+      foreground: '#FFF9C2',
       token: 'keyword.operator.or.regexp',
     },
     {
-      foreground: '#DCDCAA',
+      foreground: '#FFF9C2',
       token: 'keyword.control.anchor.regexp',
     },
     {
-      foreground: '#d7ba7d',
+      foreground: '#FFF48F',
       token: 'keyword.operator.quantifier.regexp',
     },
     {
-      foreground: '#569cd6',
+      foreground: '#3CECAF',
       token: 'constant.character',
     },
     {
-      foreground: '#d7ba7d',
+      foreground: '#FFF48F',
       token: 'constant.character.escape',
     },
     {
@@ -329,28 +329,28 @@ export const editorTheme = {
       token: 'entity.name.label',
     },
     {
-      foreground: '#569CD6',
+      foreground: '#3CECAF',
       token: 'constant.language',
     },
     {
-      foreground: '#569CD6',
+      foreground: '#3CECAF',
       token: 'entity.name.tag',
     },
     {
-      foreground: '#569cd6',
+      foreground: '#3CECAF',
       token: 'storage',
     },
   ],
   colors: {
-    'editor.foreground': semanticColors.text,
-    'editor.errorForeground': semanticColors['text-error'],
+    'editor.foreground': semanticColors['text-light'],
+    'editor.errorForeground': semanticColors['text-danger-light'],
     'editor.descriptionForeground': semanticColors['text-xlight'],
     'editor.background': semanticColors['fill-one'],
     'editor.lineHighlightBackground': semanticColors['fill-one-hover'],
     'editor.selectionBackground': semanticColors['fill-one-selected'],
     'editor.inactiveSelectionBackground': semanticColors['fill-one-hover'],
     'editorCursor.foreground': semanticColors.text,
-    'editorLineNumber.foreground': semanticColors.text,
+    'editorLineNumber.foreground': semanticColors['text-light'],
     'scrollbarSlider.background': semanticColors['fill-three'],
     'scrollbarSlider.hoverBackground': semanticColors['fill-three'],
   },
