56dc44d4e346de4813c7683a78dc6342c0a9565a
Marcin Maciaszczyk <marcin@plural.sh>
2023-01-11 12:44:20 +0100

    Use styled-components instead of honorable

--- src/stories/Date.stories.tsx
+++ src/stories/Date.stories.tsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components'
+
 import { Date } from '..'
 
 export default {
@@ -5,8 +7,36 @@ export default {
   component: Date,
 }
 
+const RootWrap = styled.div(({ theme }) => ({
+  display: 'flex',
+  gap: theme.spacing.xxxlarge,
+}))
+
+const Wrap = styled.div(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: theme.spacing.small,
+}))
+
 function Template() {
-  return (<Date date="2016-01-08T00:00:00-06:00" />)
+  return (
+    <RootWrap>
+      <Wrap>
+        <div>Date</div>
+        <Date date="2016-01-08T00:00:00-06:00" />
+      </Wrap>
+
+      <Wrap>
+        <div>Invalid date</div>
+        <Date date="invalid" />
+      </Wrap>
+
+      <Wrap>
+        <div>Undefined date</div>
+        <Date date={undefined} />
+      </Wrap>
+    </RootWrap>
+  )
 }
 
 export const Default = Template.bind({})
