02e61bc0149e7ae9aeee23d55f104a890e3c5e3b
Klink <85062+dogmar@users.noreply.github.com>
2023-02-16 12:08:29 -0800

    formatting

--- src/components/useFloatingDropdown.tsx
+++ src/components/useFloatingDropdown.tsx
@@ -1,13 +1,19 @@
-import { MutableRefObject, useMemo } from 'react';
-import { useTheme } from 'styled-components';
-import { autoUpdate, size, useFloating } from '@floating-ui/react-dom-interactions';
-import { mergeRefs } from 'react-merge-refs';
-import { SelectProps } from './Select';
+import { MutableRefObject, useMemo } from 'react'
+import { useTheme } from 'styled-components'
+import { autoUpdate, size, useFloating } from '@floating-ui/react-dom-interactions'
+import { mergeRefs } from 'react-merge-refs'
+
+import { SelectProps } from './Select'
 
 export function useFloatingDropdown({
-  placement, triggerRef, width, maxHeight,
-}: Pick<SelectProps, 'placement' | 'width' | 'maxHeight'> & { triggerRef: MutableRefObject<any>; }) {
-  const theme = useTheme();
+  placement,
+  triggerRef,
+  width,
+  maxHeight,
+}: Pick<SelectProps, 'placement' | 'width' | 'maxHeight'> & {
+  triggerRef: MutableRefObject<any>
+}) {
+  const theme = useTheme()
 
   const floating = useFloating({
     placement: `bottom-${placement === 'left' ? 'start' : 'end'}`,
@@ -15,27 +21,31 @@ export function useFloatingDropdown({
     middleware: [
       size({
         apply(args) {
-          const { elements, availableHeight, rects } = args;
+          const { elements, availableHeight, rects } = args
 
-          const maxH = typeof maxHeight === 'string'
-            ? maxHeight
-            : maxHeight
-              ? Math.min(availableHeight - theme.spacing.xxsmall, maxHeight)
-              : Math.min(availableHeight - theme.spacing.xxsmall, 230);
+          const maxH
+            = typeof maxHeight === 'string'
+              ? maxHeight
+              : maxHeight
+                ? Math.min(availableHeight - theme.spacing.xxsmall, maxHeight)
+                : Math.min(availableHeight - theme.spacing.xxsmall, 230)
 
           Object.assign(elements.floating.style, {
-            maxWidth: typeof width === 'string' && width
-              ? width
-              : `${typeof width === 'number' ? width : rects.reference.width}px`,
+            maxWidth:
+              typeof width === 'string' && width
+                ? width
+                : `${
+                  typeof width === 'number' ? width : rects.reference.width
+                }px`,
             maxHeight: `${maxH}px`,
-          });
+          })
         },
       }),
     ],
     whileElementsMounted: autoUpdate,
-  });
+  })
   const mergedRef = useMemo(() => mergeRefs([floating.reference, triggerRef]),
-    [floating.reference, triggerRef]);
+    [floating.reference, triggerRef])
 
-  return { floating, triggerRef: mergedRef };
+  return { floating, triggerRef: mergedRef }
 }
