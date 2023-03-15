7924c462534cb5e6be8af6699f3d328d908d5e3a
Marcin Maciaszczyk <marcin@plural.sh>
2023-03-15 08:55:05 +0100

    Add types

--- src/components/pricing/constants.ts
+++ src/components/pricing/constants.ts
@@ -1,10 +1,21 @@
+import { ComponentType } from 'react'
+
 import AwsLogoIcon from '../icons/AwsLogoIcon'
 import AzureLogoIcon from '../icons/AzureLogoIcon'
+import { IconProps } from '../icons/createIcon'
 import GoogleCloudLogoIcon from '../icons/GoogleCloudLogoIcon'
 
 export const APP_PRICE = 3 // TODO: Update.
 
-export const PROVIDERS = [
+type Provider = {
+  name: string
+  id: string
+  icon: ComponentType<IconProps>
+  k8sPrice: number
+  infraPrice: number
+}
+
+export const PROVIDERS: Provider[] = [
   {
     name: 'AWS',
     id: 'aws',
