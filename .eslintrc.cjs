990cee20587d5de684a515a2125d3177d1786b9b
Klink <85062+dogmar@users.noreply.github.com>
2023-10-19 15:12:09 -0700

    lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,5 +17,7 @@ module.exports = {
       { fixStyle: 'inline-type-imports' },
     ],
     'import-newlines/enforce': 'off',
+    // Allow css prop for styled-components
+    'react/no-unknown-property': ['error', { ignore: ['css'] }],
   },
 }
