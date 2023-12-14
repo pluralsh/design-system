40c76ba4cce8955ea8a86191d4a64abddc6c06a2
Klink <85062+dogmar@users.noreply.github.com>
2023-12-14 14:17:32 -0800

    Convert AppIcon to styled-components

--- src/components/AppList.tsx
+++ src/components/AppList.tsx
@@ -290,6 +290,7 @@ function AppUnstyled({
         url={logoUrl}
         icon={icon}
         size="xsmall"
+        hue="lightest"
       />
       <div className="text-container">
         <div className="title">{name}</div>
