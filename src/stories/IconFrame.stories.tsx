b877c0c6069da43180052543113dfadb094f785a
Klink <85062+dogmar@users.noreply.github.com>
2024-01-19 11:23:45 -0800

    Add ability to see IconFrames on different fill levels

--- src/stories/IconFrame.stories.tsx
+++ src/stories/IconFrame.stories.tsx
@@ -1,10 +1,37 @@
 import { Div, Flex, H1, P } from 'honorable'
 
-import { IconFrame, type IconFrameProps, TrashCanIcon } from '../index'
+import {
+  Card,
+  IconFrame,
+  type IconFrameProps,
+  TrashCanIcon,
+  WrapWithIf,
+} from '../index'
+import { type FillLevel } from '../components/contexts/FillLevelContext'
 
 export default {
   title: 'Icon Frame',
   component: IconFrame,
+  argTypes: {
+    hue: {
+      options: [undefined, 'default', 'lighter', 'lightest'],
+      control: {
+        type: 'select',
+      },
+    },
+    onFillLevel: {
+      options: [0, 1, 2, 3],
+      control: {
+        type: 'select',
+        labels: {
+          0: '0',
+          1: '1',
+          2: '2',
+          3: "3 - Shouldn't be used",
+        },
+      },
+    },
+  },
 }
 
 type Type = 'secondary' | 'tertiary' | 'floating'
@@ -26,41 +53,56 @@ function Template({
   textValue,
   tooltip,
   tooltipProps,
+  onFillLevel,
   ...props
-}: Partial<IconFrameProps>) {
-  return types.map((type) => (
-    <Div key={type}>
-      <H1
-        caption
-        marginBottom="xxsmall"
-      >
-        type="{type}"
-      </H1>
-      <Flex
-        gap="xsmall"
-        marginBottom="xlarge"
-        alignItems="center"
-        flexWrap="wrap"
-      >
-        {sizes.map((size) => (
-          <>
-            <P caption>size="{size}"</P>
-            <IconFrame
-              size={size || 'medium'}
-              clickable={clickable === undefined ? true : clickable}
-              selected={selected}
-              icon={icon || <TrashCanIcon />}
-              textValue={textValue || 'Delete'}
-              tooltip={tooltip}
-              tooltipProps={tooltipProps}
-              type={type}
-              {...props}
-            />
-          </>
-        ))}
-      </Flex>
-    </Div>
-  ))
+}: Partial<IconFrameProps> & {
+  onFillLevel: FillLevel | undefined | null
+}) {
+  return (
+    <WrapWithIf
+      condition={onFillLevel > 0}
+      wrapper={
+        <Card
+          fillLevel={onFillLevel}
+          padding="small"
+        />
+      }
+    >
+      {types.map((type) => (
+        <Div key={type}>
+          <H1
+            caption
+            marginBottom="xxsmall"
+          >
+            type="{type}"
+          </H1>
+          <Flex
+            gap="xsmall"
+            marginBottom="xlarge"
+            alignItems="center"
+            flexWrap="wrap"
+          >
+            {sizes.map((size) => (
+              <>
+                <P caption>size="{size}"</P>
+                <IconFrame
+                  size={size || 'medium'}
+                  clickable={clickable === undefined ? true : clickable}
+                  selected={selected}
+                  icon={icon || <TrashCanIcon />}
+                  textValue={textValue || 'Delete'}
+                  tooltip={tooltip}
+                  tooltipProps={tooltipProps}
+                  type={type}
+                  {...props}
+                />
+              </>
+            ))}
+          </Flex>
+        </Div>
+      ))}
+    </WrapWithIf>
+  )
 }
 
 export const Default = Template.bind({})
@@ -73,4 +115,5 @@ Default.args = {
     placement: 'top',
   },
   textValue: 'Delete',
+  onFillLevel: 0,
 }
