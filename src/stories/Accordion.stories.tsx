04aea334237c6a5f103789a8e744b1fa4b254254
Marcin Maciaszczyk <marcin@plural.sh>
2024-07-10 16:10:23 +0200

    update story

--- src/stories/Accordion.stories.tsx
+++ src/stories/Accordion.stories.tsx
@@ -1,6 +1,7 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 
-import { Accordion, Button, Input } from '..'
+import { Accordion, Button, CloseIcon, Input } from '..'
 
 export default {
   title: 'Accordion',
@@ -47,22 +48,44 @@ CustomStyling.args = {
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 }
 
-export const Horizontal = Template.bind({})
+function HorizontalTemplate({ ...args }: any) {
+  const [visible, setVisible] = useState(false)
 
-Horizontal.args = {
-  unstyled: true,
-  horizontal: true,
-  defaultOpen: true,
-  customTrigger: true,
-  customContent: true,
-  padContent: false,
-  label: 'Title',
-  children: (
-    <div>
-      <Input
-        titleContent="Test"
-        width={250}
-      />
-    </div>
-  ),
+  return (
+    <Accordion
+      {...args}
+      isOpen={visible}
+      unstyled
+      horizontal
+      triggerButton={
+        <div>
+          {!visible && (
+            <Button
+              secondary
+              backgroundColor="fill-one"
+              onClick={() => setVisible(!visible)}
+            >
+              Show
+            </Button>
+          )}
+        </div>
+      }
+    >
+      <div css={{ display: 'flex' }}>
+        <Input
+          width={250}
+          suffix={
+            <CloseIcon
+              cursor="pointer"
+              onClick={() => setVisible(false)}
+            />
+          }
+        />
+      </div>
+    </Accordion>
+  )
 }
+
+export const Horizontal = HorizontalTemplate.bind({})
+
+Horizontal.args = {}
